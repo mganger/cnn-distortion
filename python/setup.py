9a278b85fb94637b647d2b36efcc517565020864
Michael Ganger <mganger747@gmail.com>
2019-10-31 21:08:59 -0400

    Added requirements, license, and author to python package

--- python/setup.py
+++ python/setup.py
@@ -4,4 +4,11 @@ setup(
 	name="cnn_cpp",
 	version="0.1",
 	packages=["cnn_cpp"],
+	license='MIT',
+	author="Michael Ganger",
+	author_email="mg@michaelganger.org",
+	install_requires=[
+		'torch',
+		'numpy',
+	],
 )
